cfc4b519ba67a008a5dc28464c6a9b02f7fcc35b
Michel Pereira <michelpereira4410@gmail.com>
2024-04-17 14:10:31 -0300

    First class: position

---
+++ src/main/java/com/io/darkfox/application/Program.java
@@ -0,0 +1,10 @@
+package com.io.darkfox.application;
+
+import com.io.darkfox.boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
---
+++ src/main/java/com/io/darkfox/boardgame/Position.java
@@ -0,0 +1,32 @@
+package com.io.darkfox.boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int rom, int column) {
+        this.row = rom;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
