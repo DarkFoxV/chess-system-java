d77509c4dffed174a72e042f51bb5315c5cde700
Michel Pereira <michelpereira4410@gmail.com>
2024-04-19 14:34:40 -0300

    Checkmate logic

--- src/main/java/com/io/darkfox/boardgame/Board.java
+++ src/main/java/com/io/darkfox/boardgame/Board.java
@@ -43,11 +43,14 @@ public class Board {
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
+
     public Piece removePiece(Position position) {
         if (!positionExists(position)) {
-            throw new BoardException("Invalid position");
+            throw new BoardException("Position not on the board");
         }
-        if (piece(position) == null) return null;
+        if (piece(position) == null) {
+            return null;
+        }
         Piece aux = piece(position);
         aux.position = null;
         pieces[position.getRow()][position.getColumn()] = null;
